6ef3dc922cbd2271cb07f2b8e94eaec06970892f
Ahitagni Bhattacharya <ahitagnistudies@gmail.com>
2026-09-15 09:58:12 +0530

    Add functions to insert and display linked list nodes
    Implement linked list insertion functions and display.

---
+++ LinkedLIST/Insertafternode.c
@@ -0,0 +1,66 @@
+#include 
+#include 
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* insertAtEnd(struct Node* head, int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    if (head == NULL) return newNode;
+    struct Node* temp = head;
+    while (temp->next != NULL) temp = temp->next;
+    temp->next = newNode;
+    return head;
+}
+
+struct Node* insertAfterNode(struct Node* head, int data, int target) {
+    struct Node* temp = head;
+    while (temp != NULL && temp->data != target) {
+        temp = temp->next;
+    }
+
+    if (temp == NULL) {
+        printf("Target node not found!\n");
+        return head;
+    }
+
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = temp->next;
+    temp->next = newNode;
+    return head;
+}
+
+void display(struct Node* head) {
+    struct Node* temp = head;
+    while (temp != NULL) {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+}
+
+int main() {
+    struct Node* head = NULL;
+    int value, target;
+
+    head = insertAtEnd(head, 10);
+    head = insertAtEnd(head, 20);
+    head = insertAtEnd(head, 30);
+    printf("Current ");
+    display(head);
+
+    printf("Enter new value to insert: ");
+    scanf("%d", &value);
+    printf("Enter target value to insert after: ");
+    scanf("%d", &target);
+
+    head = insertAfterNode(head, value, target);
+    display(head);
+
+    return 0;
+}
